9945de0d39a637eb4035412ab296f9f18ee8e0e3
Jean Barry <jfvbarry@gmail.com>
2023-07-17 12:50:02 -0600

    Add login page

---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodosPage from '../components/pages/TodosPage';
+
+function Dashboard() {
+  return (
+    <div>
+      <TodosPage />
+    </div>
+  );
+}
+
+export default Dashboard;
--- src/utils/validators.js
+++ src/utils/validators.js
@@ -2,8 +2,14 @@ const inputIsValid = (input) => {
   return input.length > 0;
 };
 
+const inputIsEmail = (input) => {
+  const emailRegex = /\S+@\S+\.\S+/;
+  return emailRegex.test(input);
+};
+
 const validators = {
   inputIsValid,
+  inputIsEmail,
 };
 
 export default validators;
