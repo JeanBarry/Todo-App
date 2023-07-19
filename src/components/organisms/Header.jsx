5263b85fc4aed30c54d9e280a3e88b81562fba9a
Jean Barry <jfvbarry@gmail.com>
2023-07-19 01:12:04 -0600

    Add firebase

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { signOut } from 'firebase/auth';
+import { auth } from '../../config/firebase';
 import styles from './Header.module.css';
 
 function Header() {
@@ -6,6 +8,15 @@ function Header() {
     <header>
       <nav className={styles.navbar}>
         <div className={styles.navbar__item}>Todos</div>
+        <button
+          type="button"
+          className={`${styles.navbar__item} ${styles.button}`}
+          onClick={() => {
+            signOut(auth);
+          }}
+        >
+          Logout
+        </button>
       </nav>
     </header>
   );
--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
 import styles from './LoginPage.module.css';
 import Input from '../atoms/Input';
 import Button from '../atoms/Button';
 import validators from '../../utils/validators';
+import { auth } from '../../config/firebase';
 
 function LoginPage() {
   const [email, setEmail] = useState('');
@@ -10,6 +12,31 @@ function LoginPage() {
   const [password, setPassword] = useState('');
   const [passwordError, setPasswordError] = useState('');
   const [isLoggingIn, setIsLoggingIn] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        window.location.href = '/dashboard';
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const loginErrorHandler = (errorCode) => {
+    switch (errorCode) {
+      case 'auth/user-disabled':
+        setEmailError('The user has been disabled');
+        break;
+      case 'auth/user-not-found':
+      case 'auth/wrong-password':
+        setEmailError('The login details are incorrect');
+        break;
+      default:
+        // eslint-disable-next-line no-console
+        console.error(errorCode);
+    }
+  };
 
   const validateEmail = (emailInput) => {
     if (!validators.inputIsEmail(emailInput)) {
@@ -24,11 +51,24 @@ function LoginPage() {
       setPasswordError('The password must not be empty');
       return false;
     }
+
+    if (!validators.inputHasMinLength(passwordInput, 6)) {
+      setPasswordError('The password must be at least 6 characters long');
+      return false;
+    }
     return true;
   };
 
   const login = () => {
     setIsLoggingIn(true);
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        setIsLoggingIn(false);
+      })
+      .catch((error) => {
+        setIsLoggingIn(false);
+        loginErrorHandler(error.code);
+      });
   };
 
   const fontSize = '1.2rem';
@@ -63,6 +103,13 @@ function LoginPage() {
             width="100%"
             fontSize={fontSize}
             disabled={isLoggingIn}
+            onKeyDown={(event) => {
+              if (event.key === 'Enter') {
+                if (validateEmail(email) && validatePassword(password)) {
+                  login();
+                }
+              }
+            }}
           />
           <p className={styles.input_error}>{passwordError}</p>
         </div>
