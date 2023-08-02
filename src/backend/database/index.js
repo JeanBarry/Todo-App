bb03cc8f252131a8828c84853c047ba7db34d0fb
Jean Barry <jfvbarry@gmail.com>
2023-08-02 13:43:54 -0600

    Dockerize application

--- src/backend/database/index.js
+++ src/backend/database/index.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 export default async function connectToDatabase() {
   const { DB_HOST, DB_PASSWORD, DB_USER, DB_NAME } = process.env;
+  // TODO: Modify connection string to be able to connect to containerized database srv or default
   const connectionString = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
   try {
     await mongoose.connect(connectionString, {
